b9aa14093e94c4d8af86e8e538fcc09e700d88a2
Jaavii43 <jatoro_2001@yahoo.com.ar>
2022-09-15 12:28:49 -0300

    Secciones
    Agregue todas las secciones a Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { Seccion1Component } from './seccion1/seccion1.component';
 import { LoginComponent } from './modales/login/login.component';
+import { RedesComponent } from './redes/redes.component';
+import { Seccion2Component } from './seccion2/seccion2.component';
+import { Seccion3Component } from './seccion3/seccion3.component';
+import { Seccion4Component } from './seccion4/seccion4.component';
+import { Seccion5Component } from './seccion5/seccion5.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { LoginComponent } from './modales/login/login.component';
     NavbarComponent,
     FooterComponent,
     Seccion1Component,
-    LoginComponent
+    LoginComponent,
+    RedesComponent,
+    Seccion2Component,
+    Seccion3Component,
+    Seccion4Component,
+    Seccion5Component
   ],
   imports: [
     BrowserModule,
